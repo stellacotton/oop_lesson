078c71ea7b02933bea61a8334c918136196d2070
Stella Cotton <stella@stellacotton.com>
2013-10-08 22:12:38 -0700

    Moved magical mist

--- game.py
+++ game.py
@@ -5,45 +5,6 @@ from core import GameElement
 import sys
 from random import *
 
-# import pygame
-
-# def play_music(music_file):
-#     """
-#     stream music with mixer.music module in blocking manner
-#     this will stream the sound from disk while playing
-#     """
-#     clock = pygame.time.Clock()
-#     try:
-#         pygame.mixer.music.load(music_file)
-#         print "Music file %s loaded!" % music_file
-#     except pygame.error:
-#         print "File %s not found! (%s)" % (music_file, pygame.get_error())
-#         return
-#     pygame.mixer.music.play()
-#     while pygame.mixer.music.get_busy():
-#         # check if playback has finished
-#         clock.tick(30)
-# # pick a midi music file you have ...
-# # (if not in working folder use full path)
-# music_file = "HedwigsTheme.mid"
-# freq = 44100    # audio CD quality
-# bitsize = -16   # unsigned 16 bit
-# channels = 2    # 1 is mono, 2 is stereo
-# buffer = 1024    # number of samples
-# pygame.mixer.init(freq, bitsize, channels, buffer)
-# # optional volume 0 to 1.0
-# pygame.mixer.music.set_volume(0.8)
-# try:
-#     play_music(music_file)
-# except KeyboardInterrupt:
-#     # if user hits Ctrl/C then exit
-#     # (works only in console mode)
-#     pygame.mixer.music.fadeout(1000)
-#     pygame.mixer.music.stop()
-#     raise SystemExit
-
-
-
 #### DO NOT TOUCH ####
 GAME_BOARD = None
 DEBUG = False
@@ -343,7 +304,7 @@ def initialize():
 
     mist = Mist()
     GAME_BOARD.register(mist)
-    GAME_BOARD.set_el(1, 6, mist)
+    GAME_BOARD.set_el(4, 3 , mist)
 
 
 def keyboard_handler():
